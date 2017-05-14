fe530b0f749187a088ea61660a0489cbf1fd12dc
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2017-05-14 11:34:59 +0200

    Initial working basics with multiple configs

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -13,10 +13,10 @@ const common = {
     },
 
     plugins: [
-        new ThumbnailPlugin({
+        new ThumbnailPlugin([{
             source: './test/fixtures/pictures',
             destination: './temp/thumb'
-        })
+        }])
     ],
 
     module: {
